c6dbf78d4f512fd488390eee178633ecc8c08958
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-10 17:49:48 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,62 +1,75 @@
 import React, { useState, useEffect } from 'react';
 // import icons
 import { BsFillSunFill, BsMoonFill } from 'react-icons/bs';
+// import girl image
+import GirlImg from '../src/assets/img/girl.png';
 
 const App = () => {
   const [theme, setTheme] = useState('light');
 
   useEffect(() => {
-    // check local storage
     if (localStorage.getItem('theme') === null) {
-      // add to local storage
       localStorage.setItem('theme', 'light');
     }
   }, []);
 
   useEffect(() => {
-    // select html element
     const html = document.querySelector('html');
-    // check local storage
+
     if (localStorage.getItem('theme') === 'dark') {
-      // add class
       html.classList.add('dark');
-      // set theme state
       setTheme('dark');
     } else {
-      // remove class
       html.classList.remove('dark');
-      // set theme state
       setTheme('light');
     }
   }, [theme]);
 
-  // handler
+  // handler theme
   const handleThemeSwitch = () => {
-    // check local storage
     if (localStorage.getItem('theme') === 'light') {
-      // set theme state
       setTheme('dark');
-      // set local storage
       localStorage.setItem('theme', 'dark');
     } else {
-      // set theme state
       setTheme('light');
-      // set local storage
       localStorage.setItem('theme', 'light');
     }
   };
 
   return (
-    <div className='h-screen bg-white flex justify-center items-center dark:bg-black'>
+    <section className='w-full h-screen bg-[#e4F5eb] flex justify-center items-center dark:bg-black'>
+      {/* button */}
       <button
         onClick={handleThemeSwitch}
         className={`${
           theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'
-        } p-3 rounded-full text-2xl`}
+        } p-3 rounded-full text-2xl absolute right-6 top-6`}
       >
         {theme === 'dark' ? <BsFillSunFill /> : <BsMoonFill />}
       </button>
-    </div>
+      <div className='container mx-auto flex flex-col lg:flex-row gap-x-16'>
+        {/* text */}
+        <div className='flex-1 flex flex-col justify-center text-center lg:text-left px-4 mb-6'>
+          <h1 className='text-5xl lg:text-6xl text-[#242323] font-bold dark:text-white mb-12'>
+            Anytime,anywhere develop your skills
+          </h1>
+          <p className='dark:text-white max-w-[620px] mx-auto lg:mx-0'>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim
+            aperiam quo quae dicta obcaecati tempora reprehenderit fuga
+            doloribus. Illum odio sint et possimus natus id in repellendus
+            corporis sapiente molestias.
+          </p>
+        </div>
+        {/* image */}
+        <div className='flex-1'>
+          <img
+            className='w-full max-w-[450px] h-full max-h-[450px] mx-auto lg:mx-0'
+            src={GirlImg}
+            alt=''
+          />
+        </div>
+      </div>
+    </section>
   );
 };
 
