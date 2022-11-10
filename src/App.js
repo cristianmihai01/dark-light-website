5b71e36588f0195ce79a566ec4196155c233cfa1
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-10 16:58:55 +0200

    added

--- src/App.js
+++ src/App.js
@@ -1,66 +1,58 @@
 import React, { useState, useEffect } from 'react';
+// import icons
 import { BsFillSunFill, BsMoonFill } from 'react-icons/bs';
+
 const App = () => {
   const [theme, setTheme] = useState('light');
 
   useEffect(() => {
+    // check local storage
     if (localStorage.getItem('theme') === null) {
+      // add to local storage
       localStorage.setItem('theme', 'light');
     }
   }, []);
 
   useEffect(() => {
+    // select html element
+    const html = document.querySelector('html');
+    // check local storage
     if (localStorage.getItem('theme') === 'dark') {
-      document.querySelector('html').classList.add('dark');
+      // add class
+      html.classList.add('dark');
+      // set theme state
       setTheme('dark');
     } else {
-      document.querySelector('html').classList.remove('dark');
+      // remove class
+      html.classList.remove('dark');
+      // set theme state
       setTheme('light');
     }
   }, [theme]);
 
+  // handler
   const handleThemeSwitch = () => {
+    // check local storage
     if (localStorage.getItem('theme') === 'light') {
+      // set theme state
       setTheme('dark');
+      // set local storage
       localStorage.setItem('theme', 'dark');
     } else {
+      // set theme state
       setTheme('light');
+      // set local storage
       localStorage.setItem('theme', 'light');
     }
   };
-
-  // useEffect(() => {
-  //   if (window.matchMedia('{prefers-color-scheme: dark}').matches) {
-  //     setTheme('dark');
-  //   } else {
-  //     setTheme('light');
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   const html = document.querySelector('html');
-  //   if (localStorage.getItem('theme') === 'dark') {
-  //     html.classList.add('dark');
-  //     localStorage.setItem('theme', 'dark');
-  //   } else {
-  //     html.classList.remove('dark');
-  //     localStorage.setItem('theme', 'light');
-  //   }
-  // }, [theme]);
-
-  // const themeSwitchHandler = () => {
-  //   setTheme(
-  //     localStorage.getItem('theme') === 'dark'
-  //       ? localStorage.setItem('theme', 'light')
-  //       : localStorage.setItem('theme', 'dark')
-  //   );
-  // };
 
   return (
     <div className='h-screen bg-white flex justify-center items-center dark:bg-black'>
       <button
         onClick={handleThemeSwitch}
-        className='bg-green-200 p-4 rounded-full text-2xl transition-all'
+        className={`${
+          theme === 'dark' ? 'bg-white text-black' : 'bg-black text-white'
+        } p-3 rounded-full text-2xl`}
       >
         {theme === 'dark' ? <BsFillSunFill /> : <BsMoonFill />}
       </button>
