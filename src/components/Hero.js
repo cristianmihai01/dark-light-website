e3d471d5e28d47103f086209225bb1f8f4747ae8
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-11 11:24:14 +0200

    added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-// import icons
-import { BsFillSunFill, BsMoonFill } from 'react-icons/bs';
 // import logo
 import Logo from '../assets/img/logo.svg';
+// import icons
+import { BsFillSunFill, BsMoonFill } from 'react-icons/bs';
+// import image
+import GirlImg from '../assets/img/girl.png';
 
 const Hero = () => {
   const [theme, setTheme] = useState('light');
@@ -36,14 +38,33 @@ const Hero = () => {
     }
   };
   return (
-    <section className='h-screen w-screen bg-hero bg-cover bg-center bg-no-repeat'>
+    <section className='min-h-[740px] w-screen bg-hero bg-cover bg-center bg-no-repeat overflow-hidden'>
       <div className='container mx-auto'>
-        <header>
+        <header className='bg-amber-200'>
           {/* logo */}
           <div>
             <img src={Logo} alt='' />
           </div>
         </header>
+        {/* text & image wrapper */}
+        <div className='flex flex-col items-center lg:flex-row min-h-[740px]'>
+          {/* text */}
+          <div className='flex-1'>
+            <h1 className='text-5xl text-primary font-bold'>
+              Online Accounting <br />{' '}
+              <span className='text-accent'>Fast & Uncomplicated</span>
+            </h1>
+            <p>
+              We specialize in small businesses. Our goal is to eliminate
+              bureaucracy, creating a modern relationship between your company
+              and the accountant.
+            </p>
+          </div>
+          {/* image */}
+          <div className='self-end'>
+            <img src={GirlImg} alt='' />
+          </div>
+        </div>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@ module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{html,js}'],
   theme: {
+    fontFamily: {
+      primary: 'Rubik',
+    },
+    colors: {
+      primary: '#232129',
+      accent: '#6738E1',
+      grey: '#494455',
+    },
     extend: {
       backgroundImage: {
         hero: "url('/src/assets/img/bg.svg')",
