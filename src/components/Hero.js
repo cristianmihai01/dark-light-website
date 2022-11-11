93dac12131d2d06f4da7a763cb192b4f9de6f3ab
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-11 14:49:17 +0200

    added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 // import logo
-import Logo from '../assets/img/logo.svg';
+import LogoDark from '../assets/img/logo-dark.svg';
+import LogoWhite from '../assets/img/logo-white.svg';
 // import icons
-import { BsFillSunFill, BsMoonFill } from 'react-icons/bs';
+import { BsFillSunFill, BsMoonFill, BsCheck } from 'react-icons/bs';
+import { IoEnterOutline } from 'react-icons/io5';
 // import image
 import GirlImg from '../assets/img/girl.png';
 
@@ -38,31 +40,110 @@ const Hero = () => {
     }
   };
   return (
-    <section className='min-h-[740px] w-screen bg-hero bg-cover bg-center bg-no-repeat overflow-hidden'>
-      <div className='container mx-auto'>
-        <header className='bg-amber-200'>
-          {/* logo */}
-          <div>
-            <img src={Logo} alt='' />
+    <section className='min-h-[740px] w-screen bg-hero dark:bg-none dark:bg-dark bg-cover bg-center bg-no-repeat overflow-hidden'>
+      <div className='container mx-auto px-4 lg:px-0'>
+        {/* header */}
+        <header className='py-8 flex items-center justify-between'>
+          <div className='flex items-center'>
+            {/* logo */}
+            <div className='mr-16'>
+              {theme === 'light' ? (
+                <img src={LogoDark} alt='' />
+              ) : (
+                <img src={LogoWhite} alt='' />
+              )}
+            </div>
+            {/* nav */}
+            <nav className='hidden lg:flex gap-x-[40px] text-base dark:text-white'>
+              <a
+                className='hover:text-accent-hover transition tracking-[0.02em]'
+                href='#'
+              >
+                Solutions
+              </a>
+              <a
+                className='hover:text-accent-hover transition tracking-[0.02em]'
+                href='#'
+              >
+                Plans
+              </a>
+              <a
+                className='hover:text-accent-hover transition tracking-[0.02em]'
+                href='#'
+              >
+                About
+              </a>
+              <a
+                className='hover:text-accent-hover transition tracking-[0.02em]'
+                href='#'
+              >
+                Contact us
+              </a>
+            </nav>
+          </div>
+          {/* toolbar */}
+          <div className='flex gap-x-8 items-center'>
+            {/* log in & btn */}
+            <div className='hidden lg:flex gap-x-8'>
+              <a
+                href='#'
+                className='flex items-center gap-x-2 text-accent  hover:text-accent-hover transition'
+              >
+                <IoEnterOutline />
+                <span className='text-base'>Log in</span>
+              </a>
+              {/* btn */}
+              <button className='btn'>Discover our plans</button>
+            </div>
+            {/* light & dark btn */}
+            <button
+              onClick={handleThemeSwitch}
+              className='p-4 bg-accent text-white rounded-full w-12 h-12 flex justify-center items-center'
+            >
+              {theme === 'light' ? <BsMoonFill /> : <BsFillSunFill />}
+            </button>
           </div>
         </header>
         {/* text & image wrapper */}
         <div className='flex flex-col items-center lg:flex-row min-h-[740px]'>
           {/* text */}
-          <div className='flex-1'>
-            <h1 className='text-5xl text-primary font-bold'>
-              Online Accounting <br />{' '}
+          <div className='flex-1 flex flex-col justify-center items-start'>
+            <h1 className='text-5xl text-primary font-bold mb-6 leading-[60px] dark:text-white'>
+              Online Accounting <br />
               <span className='text-accent'>Fast & Uncomplicated</span>
             </h1>
-            <p>
+            <p className='font-light text-grey mb-12 max-w-[597px] dark:text-white'>
               We specialize in small businesses. Our goal is to eliminate
               bureaucracy, creating a modern relationship between your company
               and the accountant.
             </p>
+            {/* checked items */}
+            <div className='flex flex-col gap-y-6 mb-12'>
+              <div className='flex items-center gap-x-2'>
+                <div className='bg-accent/10 text-accent w-[20px] h-[20px] rounded-full flex justify-center items-center dark:bg-accent/70 dark:text-white'>
+                  <BsCheck />
+                </div>
+                <p className='text-base font-light dark:text-white'>
+                  Have your accounting 100% online.
+                </p>
+              </div>
+              <div className='flex items-center gap-x-2'>
+                <div className='bg-accent/10 text-accent w-[20px] h-[20px] rounded-full flex justify-center items-center dark:bg-accent/70 dark:text-white'>
+                  <BsCheck />
+                </div>
+                <p className='text-base font-light dark:text-white'>
+                  Save with plans starting at $10/month.
+                </p>
+              </div>
+            </div>
+            {/* button */}
+            <button className='btn'>Discover our plans</button>
           </div>
           {/* image */}
-          <div className='self-end'>
+          <div className='self-end hidden lg:flex relative'>
             <img src={GirlImg} alt='' />
+            <div className='absolute right-24'>icon 1</div>
+            <div className='absolute right-0'>icon 2</div>
           </div>
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,28 @@ module.exports = {
     fontFamily: {
       primary: 'Rubik',
     },
-    colors: {
-      primary: '#232129',
-      accent: '#6738E1',
-      grey: '#494455',
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        lg: '0',
+      },
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1224px',
+      xl: '1350px',
     },
     extend: {
+      colors: {
+        primary: '#232129',
+        accent: {
+          DEFAULT: '#6738E1',
+          hover: '#5A26DF',
+        },
+        grey: '#494455',
+        dark: '#131014',
+      },
       backgroundImage: {
         hero: "url('/src/assets/img/bg.svg')",
       },
